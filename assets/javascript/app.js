601746c1e185c3e2e08767b3d8573e7ab013b628
Sarah Arnold <digipet007@gmail.com>
2019-08-24 19:51:22 -0700

    replaced classic for loop with 'for (var key in questions[i].answers)'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,18 +28,17 @@ var questions = [{
 
 //-------FUNCTIONS------------------------------------------------------
 $("#startBtn").on("click", function(){
-    $("#startBtn").remove();
+    $("#startBtn").remove(); 
     //loop through the array of objects to render all questions to the page in their own div.
     for (var i = 0; i < questions.length; i++){
-        $("#questionsContainer").append("<div class='qA'>" + "<h2>" + questions[i].question + "</h2>" + "</div>") 
-        // .append("<input type='radio' name='question-"+i+"' value='" + questions[i].answers+"'> "+questions[i].answers);
-        console.log(questions[i].answers.length);
-        for (var j = 0; j < 4; i++){
-            console.log(questions[i]);
+        var questDiv = $("<div class='qA'>" + "<h2>" + questions[i].question + "</h2>" + "</div>");
+        $("#questionsContainer").append(questDiv)
+        for (var key in questions[i].answers){
             //radio input type means you can only select one of the options
             //append each question with a name equal to the number of the question and a value that is equal to the answer
             //the values will be stored in the buttons
-            $("#qA").append("<input type='radio' name='question-"+i+"' value='" + questions[i].answers+"'>"+questions[i].answers);
+            var ans = ("<input type='radio' name='question-"+i+"' value='" + questions[i].answers[key]+"'> "+questions[i].answers[key]);
+            (questDiv).append(ans);
         }
     }
 })
